7e2ba1995622255828571bdcead3742a28253242
Maxim Saplin <smaxmail@gmail.com>
2024-12-09 17:56:01 +0300

    Styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,13 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary')}>
       <div className="container">
-        <Heading as="h1" style={{fontSize: '1000%'}}>
-          <span style={{opacity: '0.25'}}>&lt;</span>&nbsp;
+        <Heading as="h1" style={{fontSize: '820%'}}>
+          <span style={{opacity: '0.25'}}>&lt;&nbsp;</span>
           x
           <strong>dlc</strong>
           .ai
-          &nbsp;
-          <span style={{opacity: '0.25'}}>/&gt;</span>
+          
+          <span style={{opacity: '0.25'}}>&nbsp;/&gt;</span>
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
